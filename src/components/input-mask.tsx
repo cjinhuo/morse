192675fc316ee98c362024607856af2ffe190c05
Shanks <cjinhuo@qq.com>
2024-08-14 16:29:32 +0800

    feat: let space button out for temporary

--- src/components/input-mask.tsx
+++ src/components/input-mask.tsx
@@ -1,12 +1,12 @@
 import { useAtomValue } from 'jotai'
-import { InputtingMorseCodeAtom } from '../atom/atom'
-import styled from 'styled-components'
-import { TYPING_STATUS } from '../shared/constants'
 import { useMemo } from 'react'
+import styled from 'styled-components'
+import { InputtingMorseCodeAtom } from '../atom/atom'
+import useMobile from '../hooks/useMobile'
+import { TYPING_STATUS } from '../shared/constants'
 import { transformMorseCodeToChar } from '../shared/utils'
 import MorseCodeSvg from './morse-code-svg'
 import MotionSpaceButton from './motion-space-button'
-import useMobile from '../hooks/useMobile'
 
 const InputMaskContainer = styled.div`
   position: fixed;
@@ -44,29 +44,30 @@ const InputMaskContainer = styled.div`
 `
 
 export default function InputMask() {
-  const { morseCode, status } = useAtomValue(InputtingMorseCodeAtom)
-  const isMobile = useMobile()
-  const [blockClassName, targetChar] = useMemo(() => {
-    if (status === TYPING_STATUS.idle) return ['invisible']
-    if (status === TYPING_STATUS.done) {
-      return ['visibility-hidden', transformMorseCodeToChar(morseCode)]
-    }
-    return ['visibility-show']
-  }, [status, morseCode])
+	const { morseCode, status } = useAtomValue(InputtingMorseCodeAtom)
+	const isMobile = useMobile()
+	const [blockClassName, targetChar] = useMemo(() => {
+		if (status === TYPING_STATUS.idle) return ['invisible']
+		if (status === TYPING_STATUS.done) {
+			return ['visibility-hidden', transformMorseCodeToChar(morseCode)]
+		}
+		return ['visibility-show']
+	}, [status, morseCode])
 
-  return (
-    <InputMaskContainer>
-      <div
-        style={{ backgroundColor: 'var(--color-neutral-8)', opacity: '0.8' }}
-        className={`text-3xl overflow-hidden w-full h-16 flex justify-center items-center rounded-2xl ${blockClassName}`}>
-        <MorseCodeSvg morseCode={morseCode}></MorseCodeSvg>
-        <div className='text-skin-neutral-4'>{targetChar === null ? '-> invalid morse code' : targetChar}</div>
-      </div>
-      {isMobile && (
-        <div className='h-16 w-full mt-4'>
-          <MotionSpaceButton></MotionSpaceButton>
-        </div>
-      )}
-    </InputMaskContainer>
-  )
+	return (
+		<InputMaskContainer>
+			<div
+				style={{ backgroundColor: 'var(--color-neutral-8)', opacity: '0.8' }}
+				className={`text-3xl overflow-hidden w-full h-16 flex justify-center items-center rounded-2xl ${blockClassName}`}
+			>
+				<MorseCodeSvg morseCode={morseCode} />
+				<div className='text-skin-neutral-4'>{targetChar === null ? '-> invalid morse code' : targetChar}</div>
+			</div>
+			{/* {isMobile && ( */}
+			<div className='h-16 w-full mt-4'>
+				<MotionSpaceButton />
+			</div>
+			{/* )} */}
+		</InputMaskContainer>
+	)
 }
--- src/components/morse-typer.tsx
+++ src/components/morse-typer.tsx
@@ -1,66 +1,66 @@
-import { useEffect, useRef } from 'react'
-import { CHAR_STATUS, TYPING_STATUS } from '../shared/constants'
-import { InputtingMorseCodeAtom } from '../atom/atom'
+import { motion } from 'framer-motion'
 import { useSetAtom } from 'jotai'
+import { useEffect, useRef } from 'react'
+import { InputtingMorseCodeAtom } from '../atom/atom'
+import { CHAR_STATUS, TYPING_STATUS } from '../shared/constants'
 import { getOscillatorNodeWithParams, subscribeKeyEventForMorseCode, transformMorseCodeToChar } from '../shared/utils'
 import PureTypeChar, { type RefMethodsType } from './pure-type-char'
-import { motion } from 'framer-motion'
 
 export default function MorseTyper() {
-  const setCurrentMorseCode = useSetAtom(InputtingMorseCodeAtom)
-  const pureTypeCharRef = useRef<RefMethodsType | null>(null)
+	const setCurrentMorseCode = useSetAtom(InputtingMorseCodeAtom)
+	const pureTypeCharRef = useRef<RefMethodsType | null>(null)
 
-  useEffect(() => {
-    if (!pureTypeCharRef.current) return
+	useEffect(() => {
+		if (!pureTypeCharRef.current) return
 
-    let currentElement = pureTypeCharRef.current.start()
-    const [$singleChar, $fragment] = subscribeKeyEventForMorseCode(getOscillatorNodeWithParams)
-    const singleCharSubscription = $singleChar.subscribe((char) => {
-      setCurrentMorseCode((prev) => {
-        return prev.status !== TYPING_STATUS.typing
-          ? {
-              status: TYPING_STATUS.typing,
-              morseCode: char,
-            }
-          : {
-              status: TYPING_STATUS.typing,
-              morseCode: prev.morseCode + char,
-            }
-      })
-    })
-    const fragmentSubscription = $fragment.subscribe((fragment) => {
-      const char = transformMorseCodeToChar(fragment)
-      if (char) {
-        if (currentElement?.innerHTML === char) {
-          ;[currentElement] = pureTypeCharRef.current!.next(CHAR_STATUS.correct)
-        } else {
-          ;[currentElement] = pureTypeCharRef.current!.next(CHAR_STATUS.error, char)
-        }
-      }
+		let currentElement = pureTypeCharRef.current.start()
+		const [$singleChar, $fragment] = subscribeKeyEventForMorseCode(getOscillatorNodeWithParams)
+		const singleCharSubscription = $singleChar.subscribe((char) => {
+			setCurrentMorseCode((prev) => {
+				return prev.status !== TYPING_STATUS.typing
+					? {
+							status: TYPING_STATUS.typing,
+							morseCode: char,
+						}
+					: {
+							status: TYPING_STATUS.typing,
+							morseCode: prev.morseCode + char,
+						}
+			})
+		})
+		const fragmentSubscription = $fragment.subscribe((fragment) => {
+			const char = transformMorseCodeToChar(fragment)
+			if (char) {
+				if (currentElement?.innerHTML === char) {
+					;[currentElement] = pureTypeCharRef.current!.next(CHAR_STATUS.correct)
+				} else {
+					;[currentElement] = pureTypeCharRef.current!.next(CHAR_STATUS.error, char)
+				}
+			}
 
-      setCurrentMorseCode((prev) => ({
-        status: TYPING_STATUS.done,
-        morseCode: prev.morseCode,
-      }))
-      if (currentElement) {
-        // skip when encounter space char
-        if (currentElement.innerText === ' ') {
-          ;[currentElement] = pureTypeCharRef.current!.next(CHAR_STATUS.correct)
-        }
-      } else {
-        singleCharSubscription.unsubscribe()
-        fragmentSubscription.unsubscribe()
-        console.log('ALL over')
-      }
-    })
-    return () => {
-      singleCharSubscription.unsubscribe()
-      fragmentSubscription.unsubscribe()
-    }
-  }, [])
-  return (
-    <div>
-      <PureTypeChar data='HELLO WORLD' ref={pureTypeCharRef}></PureTypeChar>
-    </div>
-  )
+			setCurrentMorseCode((prev) => ({
+				status: TYPING_STATUS.done,
+				morseCode: prev.morseCode,
+			}))
+			if (currentElement) {
+				// skip when encounter space char
+				if (currentElement.innerText === ' ') {
+					;[currentElement] = pureTypeCharRef.current!.next(CHAR_STATUS.correct)
+				}
+			} else {
+				singleCharSubscription.unsubscribe()
+				fragmentSubscription.unsubscribe()
+				console.log('ALL over')
+			}
+		})
+		return () => {
+			singleCharSubscription.unsubscribe()
+			fragmentSubscription.unsubscribe()
+		}
+	}, [])
+	return (
+		<div>
+			<PureTypeChar data='HELLO WORLD' ref={pureTypeCharRef}></PureTypeChar>
+		</div>
+	)
 }
